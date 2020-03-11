e7315449b114ad96179c4c3e3acd4758213f9084
Saurabh Jaiswal <saurabhchris1@gmail.com>
2020-03-11 14:48:10 -0700

    Added Axios package and Firebase integration

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -79,7 +80,26 @@ class BurgerBuilder extends Component{
     }
 
     purchaseContinueHandler = ()=>{
-      alert("You Continue");
+     // alert("You Continue");
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer:{
+                name: 'Saurabh Jaiswal',
+                address:{
+                    street: '880 NE Providence Ct',
+                    zipCode: '99163',
+                    country: 'Germany'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json', order).then(response => {
+            console.log(response);
+        }).catch(error => {
+            console.log(error);
+        });
     };
 
     render() {
